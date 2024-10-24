b88c28a3fcc77c76cca2f9950a08da4e44f0d911
JungSeongKwon <Jungsam000@gmail.com>
2024-10-24 20:53:32 +0900

    1024_Upload
    + Day 1 (~ P.53)

---
+++ Chapter02/EX_1/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// 다음과 같이 텍스트를 출력하는 프로그램을 작성하세요.
+// 여러분, 안녕하세요?
+// 반갑습니다!
+
+namespace EX_1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?\n반갑습니다!");
+        }
+    }
+}
