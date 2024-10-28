68e20c882b757e6f83b78073c3d317a0401aadc0
JungSeongKwon <Jungsam000@gmail.com>
2024-10-28 20:05:37 +0900

    1028_Upload
    + Day5 (P126 ~ P153, 45m)

---
+++ Chapter04/07_NullConditionalOperator/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace _07_NullConditionalOperator
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구");           // a가 null을 반환하므로 Add() 메소드는 호출되지 않음
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList();    // a는 더 이상 null이 아닙니다.
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+        }
+    }
+}
---
+++ Chapter04/11_NullCoalescing/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _11_NullCoalescing
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.WriteLine($"{num ?? 0}");
+
+            num = 99;
+            Console.WriteLine($"{num ?? 0}");
+
+            string str = null;
+            Console.WriteLine($"{str ?? "Default"}");
+            
+            str = "Specific";
+            Console.WriteLine($"{str ?? "Default"}");
+        }
+    }
+}
