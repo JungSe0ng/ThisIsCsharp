6c6310682ba0d7f31d1ef292334855a4f018e7d0
JungSeongKwon <Jungsam000@gmail.com>
2024-11-01 18:14:08 +0900

    1031_Upload
    + Day8 (P179 ~ P224, 62m)

---
+++ Chapter06/06_UsingOut/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06_UsingOut
+{
+    class MainApp
+    {
+
+        static void Divide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 3;
+            //int c;
+            //int d;
+
+            Divide(a, b, out int c, out int d);
+
+            Console.WriteLine($"a : {a}, b : {b}, c : {c}, d : {d}");
+        }
+    }
+}
