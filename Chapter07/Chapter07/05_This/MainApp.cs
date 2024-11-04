47d103c78b0e888d2d76eda2adf5804662099b40
JungSeongKwon <Jungsam000@gmail.com>
2024-11-04 20:40:41 +0900

    1104_Upload
    + Day10 (P254 ~ P275, 30m)

---
+++ Chapter07/Chapter07/05_This/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05_This
+{
+    class Employee
+    {
+        private string Name;
+        private string Position;
+        public void SetNmae(string Name)
+        {
+            this.Name = Name;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetPosition(string Position) 
+        {
+            this.Position = Position;
+        }
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetNmae("Pooh");
+            pooh.SetPosition("Waiter");
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+            Employee tigger = new Employee();
+            tigger.SetNmae("Tigger");
+            tigger.SetPosition("Cleaner");
+            Console.WriteLine($"{tigger.GetName()} {tigger.GetPosition()}");
+        }
+    }
+}
