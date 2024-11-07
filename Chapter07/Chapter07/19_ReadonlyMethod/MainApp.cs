72d93731195285200b545afd267da6f7ffe0c92b
JungSeong <jungsam000@gmail.com>
2024-11-07 22:55:49 +0900

    1107_Upload
    + Day15 (P301 ~ P311, 35m)

---
+++ Chapter07/Chapter07/19_ReadonlyMethod/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _19_ReadonlyMethod
+{
+    struct ACSetting
+    {
+        public double currentInCelsius;
+        public double target;
+
+        public readonly double GetFahrenheit()
+        {
+            // 화씨 계산 결과를 target에 저장
+            target = currentInCelsius * 1.8 + 32;
+            return target;  // target 반환
+        }
+    }
+    
+    class MainAPp
+    {
+        static void Main(string[] args)
+        {
+            ACSetting acs;
+            acs.currentInCelsius = 25;
+            acs.target = 25;
+
+            Console.WriteLine($"{acs.GetFahrenheit()}");
+            Console.WriteLine($"{acs.target}");
+        }
+    }
+}
