336187cf1ebe0a8598348dde782e809895e2ee4a
JungSeongKwon <Jungsam000@gmail.com>
2024-11-12 18:44:47 +0900

    + Day18 (P347 ~ P364, 48m)

---
+++ Chapter09/03_ConstructWithProperty/MainApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _03_ConstructWithProperty
+{
+    class BirthdayInfo
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public DateTime BirthDay
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(BirthDay).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo()
+            {
+                Name = "서현",
+                BirthDay = new DateTime(1991, 6, 28)
+            };
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"BirthDay : {birth.BirthDay.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+}
---
+++ Chapter09/05_RequiredProperty/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05_RequiredProperty
+{
+    class BirthdayInfo
+    {
+        public required string Name { get; set; }
+        public required DateTime Birthday { get; set; }
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo() { Name = "서현", Birthday = new DateTime(1991, 6, 28) };
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+}
