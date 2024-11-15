32bd484e038d17ca8ce726e96505185f86002684
JungSeongKwon <Jungsam000@gmail.com>
2024-11-15 20:02:40 +0900

    + Day21 (P365 ~ P386, 37m)

---
+++ Chapter10/04_DerivedFromArray/MainApp.cs
@@ -0,0 +1,12 @@
+﻿namespace _04_DerivedFromArray
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 20, 30,40,50 };
+            Console.WriteLine($"Type Of array : {array.GetType()}");
+            Console.WriteLine($"Base type Of array : {array.GetType().BaseType}");
+        }
+    }
+}
