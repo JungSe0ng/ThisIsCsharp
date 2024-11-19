d246791edc2fa14473c969a1eaecfdc1b2f5fdf5
JungSeongKwon <Jungsam000@gmail.com>
2024-11-19 21:00:01 +0900

    + Day23 (P442 ~ P459, 31m)

---
+++ Chapter12/01_KillingProgram/MainApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_KillingProgram
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(arr[i]);
+
+            Console.WriteLine("종료");
+        }
+    }
+}
